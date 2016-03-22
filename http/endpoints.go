4e1ba6f59adf79a3f6b407c892bf6a7ebc65c0c0
Josh Roppo <joshroppo@gmail.com>
2016-03-22 14:50:33 -0700

    Turning the nodestatus function into a closure returning a function
    This is necessary to pass the hostname parameter defined from an
    entrypoint.

--- http/endpoints.go
+++ http/endpoints.go
@@ -10,38 +10,50 @@ import (
 	"github.com/lytics/gowrapmx4j"
 )
 
-func NodeStatus(w http.ResponseWriter, r *http.Request) {
-	mjs := make(map[string]interface{})
-	nsb := gowrapmx4j.RegistryGet("NodeStatusBinary")
-	metricMap, err := gowrapmx4j.DistillAttributeTypes(nsb.Data)
-	if err != nil {
-		mjs["ERR"] = "Error extracting node status data"
-		mjs["error"] = fmt.Sprintf("%v", err)
-	}
+func NodeStatus(hostname string) func(w http.ResponseWriter, r *http.Request) {
 
-	states, ok := metricMap["SimpleStates"]
-	log.Debugf("%#v", states)
-	if !ok {
-		mjs["ERR"] = "Error extracting node status data"
-		mjs["error"] = "Key: SimpleStates not in data map"
-	}
-	ss := states.(map[string]interface{})
-
-	var hostKey string
-	for k, v := range ss {
-		log.Debug("Nodestatus: %s %#v", k, v)
-		//TODO: Define hostnameid
-		hostMatch := regexp.MustCompile(fmt.Sprintf(".*%s.*", hostnameid))
-		if hostMatch.MatchString(k) {
-			hostKey = k
+	return func(w http.ResponseWriter, r *http.Request) {
+		mjs := make(map[string]interface{})
+		nsb := gowrapmx4j.RegistryGet("NodeStatusBinary")
+		metricMap, err := gowrapmx4j.DistillAttributeTypes(nsb.Data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("nodeStatus: Error extracting node status data: %v", err), 500)
+			return
 		}
-	}
 
-	mjs[hostKey] = ss[hostKey]
-	js, err := json.Marshal(mjs)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, fmt.Sprintf("nodeStatus: Error marshaling JSON from MX4J data: %#v", err), 500)
+		states, ok := metricMap["SimpleStates"]
+		log.Debugf("%#v", states)
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("nodeStatus: Error extracting node status data;  Key: SimpleStates not in data map"), 500)
+			return
+		}
+		ss := states.(map[string]interface{})
+
+		var hostKey string
+		hostMatch := regexp.MustCompile(fmt.Sprintf(".*%s.*", hostname))
+		for k, v := range ss {
+			log.Debug("Nodestatus: %s %#v", k, v)
+			if hostMatch.MatchString(k) {
+				hostKey = k
+				break
+			}
+		}
+
+		mjs[hostKey], ok = ss[hostKey]
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("nodeStatus: Error finding hostname=\"%s\" as key in list of nodes: %#v", hostname, err), 500)
+			return
+		}
+
+		js, err := json.Marshal(mjs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("nodeStatus: Error marshaling JSON from MX4J data: %#v", err), 500)
+			return
+		}
+		fmt.Fprintf(w, "%s", js)
 	}
-	fmt.Fprintf(w, "%s", js)
 }
